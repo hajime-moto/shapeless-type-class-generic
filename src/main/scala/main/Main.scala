0db135e091e72a541e042b075f5f54caeb72eb1a
hajime-moto <hajime-moto@users.noreply.github.com>
2016-06-15 19:20:58 +1000

    removed CommandHandlerRunner

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,6 +1,6 @@
 package main
 
-import user.{CreateUser, UpdatePassword, UserCommand}
+import user.{CommandHandlerRunner, CreateUser, UpdatePassword, UserCommand}
 import util.CommandHandler
 
 object Main extends App {
@@ -8,11 +8,6 @@ object Main extends App {
   import shapeless._
   import UserCommand._
   import CommandHandler._
-
-  object CommandHandlerRunner {
-    def processCommand[C](command: C)(implicit commandHandler: CommandHandler[C]) =
-      commandHandler.processCommand(command)
-  }
 
   val cmd1: UserCommand = CreateUser("foo")
   val cmd2 = UpdatePassword("id", "open sesame")
